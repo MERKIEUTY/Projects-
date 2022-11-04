7698da15361af9c6e66737bde62bdc3fb8b5637d
Merkieuty <merkiejade@gmail.com>
2022-11-04 20:16:06 +0800

    first commit

---
+++ Trip/TRIP/Form1.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.YES = New System.Windows.Forms.Button()
+        Me.NO = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(-7, 81)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(366, 39)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "HI! ARE YOU OKAY?"
+        '
+        'YES
+        '
+        Me.YES.Location = New System.Drawing.Point(12, 222)
+        Me.YES.Name = "YES"
+        Me.YES.Size = New System.Drawing.Size(111, 38)
+        Me.YES.TabIndex = 1
+        Me.YES.Text = "YES"
+        Me.YES.UseVisualStyleBackColor = True
+        '
+        'NO
+        '
+        Me.NO.Location = New System.Drawing.Point(228, 222)
+        Me.NO.Name = "NO"
+        Me.NO.Size = New System.Drawing.Size(108, 38)
+        Me.NO.TabIndex = 2
+        Me.NO.Text = "NO"
+        Me.NO.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(72, 186)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(210, 20)
+        Me.TextBox1.TabIndex = 3
+        Me.TextBox1.Visible = False
+        '
+        'Button1
+        '
+        Me.Button1.DialogResult = System.Windows.Forms.DialogResult.Yes
+        Me.Button1.Location = New System.Drawing.Point(124, 282)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(92, 56)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "ENTER"
+        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.Visible = False
+        '
+        'Button2
+        '
+        Me.Button2.DialogResult = System.Windows.Forms.DialogResult.Yes
+        Me.Button2.Location = New System.Drawing.Point(124, 282)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(92, 56)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "ENTER"
+        Me.Button2.UseVisualStyleBackColor = True
+        Me.Button2.Visible = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.TRIP.My.Resources.Resources.hug_love
+        Me.PictureBox2.Location = New System.Drawing.Point(62, 134)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(220, 179)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox2.TabIndex = 7
+        Me.PictureBox2.TabStop = False
+        Me.PictureBox2.Visible = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.TRIP.My.Resources.Resources.asd
+        Me.PictureBox1.Location = New System.Drawing.Point(62, 123)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(220, 220)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 6
+        Me.PictureBox1.TabStop = False
+        Me.PictureBox1.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(348, 412)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.NO)
+        Me.Controls.Add(Me.YES)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents YES As Button
+    Friend WithEvents NO As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+End Class
---
+++ Trip/TRIP/Form1.vb
@@ -0,0 +1,53 @@
+﻿Public Class Form1
+    Private Sub NO_Click(sender As Object, e As EventArgs) Handles NO.Click
+
+        NO.Hide()
+        YES.Hide()
+        Label1.Text = "WHAT SHOULD I CALL YOU?"
+        TextBox1.Show()
+        Button1.Show()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim name As String
+
+        name = TextBox1.Text
+        TextBox1.Text = name
+        MsgBox(" HI " + name + " HOW'S LIFE ")
+        MsgBox("REMEMBER I AM ALWAYS HERE FOR YOU")
+        MsgBox("BELIEVE IN YOURSELF")
+        MsgBox("I KNOW YOU CAN DO IT")
+        MsgBox("GOODLUCK")
+    End Sub
+
+    Private Sub YES_Click(sender As Object, e As EventArgs) Handles YES.Click
+        NO.Hide()
+        YES.Hide()
+
+        TextBox1.Show()
+        Button2.Show()
+
+        NO.Hide()
+        YES.Hide()
+
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim name As String
+
+        name = TextBox1.Text
+        NO.Hide()
+
+        YES.Hide()
+        Button2.Hide()
+
+        Label1.Text = " HI " + name + ""
+        name = TextBox1.Text
+        TextBox1.Text = name
+        PictureBox2.Show()
+        TextBox1.Hide()
+    End Sub
+End Class
